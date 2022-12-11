64349160eccb7d71dc79b92ffc06b9459958291a
Swati Beniwal <swatibeniwal95@gmail.com>
2022-12-10 20:58:22 -0800

    l

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,4 +5,21 @@
  * @list: check linkes list
  * Return: 0 if no cycle and else 1
  */
-int check_cycle
+int check_cycle(listint_t *list)
+{
+	listint_t *sl = list;
+	listint_t *ft = list;
+
+	if (!list)
+		return(0);
+
+	while (sl && ft && ft->next)
+	{
+		sl = sl->next;
+		ft = ft->next->next;
+		if (sl == ft)
+			return (1);
+	}
+
+	return (0);
+}
